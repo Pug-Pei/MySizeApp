a16c0b334ff42d9b61586f9101254969a1370ed0
kouhei <grimpoteuthistako@gmail.com>
2022-03-21 15:24:15 +0900

    Initial commit

---
+++ app/src/main/java/co/websarva/wings/android/mysizeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.websarva.wings.android.mysizeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
